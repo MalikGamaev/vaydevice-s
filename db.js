14c894377a27c25c0609b0ca65b7c5fb3a65e471
MalikGamaev <malikgamaev@icloud.com>
2024-10-20 00:26:52 +0300

    update

--- db.js
+++ db.js
@@ -1,13 +1,22 @@
 const { Sequelize } = require('sequelize');
 
 
-module.exports = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_USER,
-	process.env.DB_PASSWORD,
+module.exports = new Sequelize('postgresql://vaydevice_database_user:bTqZAgJeIiKJYP9oyaAAo3sOg9pZVjZC@dpg-csa20ja3esus739ofaa0-a.oregon-postgres.render.com/vaydevice_database?sslmode=no-verify',
 	{
-		dialect: 'postgres',
-		host: process.env.DB_HOST,
-		port: process.env.DB_PORT,
+		pool: {
+			max: 3,
+			min: 1,
+			idle: 10000,
+		},
+		dialectOptions: {
+			ssl: {
+				require: true
+			},
+			keepAlive: true
+		}
 	}
+
 )
+
+//malikgamaev @icloud.com
+//e1e71b174c3357ef02ecbc654fe989c9

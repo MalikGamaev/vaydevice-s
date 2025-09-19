e723f4c9ff51bb8c954572273a07094b1aa3f582
MalikGamaev <malikgamaev@icloud.com>
2025-09-19 13:32:23 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
 const { Sequelize } = require('sequelize');
 
 
-module.exports = new Sequelize('postgresql://vaydevice_database_user:bTqZAgJeIiKJYP9oyaAAo3sOg9pZVjZC@dpg-csa20ja3esus739ofaa0-a.oregon-postgres.render.com/vaydevice_database?',
+module.exports = new Sequelize('postgresql://vaydevice_database_sbmv_user:hkHEhyKeTFhK7r26Z1FfpGNgcW4fHUfn@dpg-d36j0g3uibrs739cqvkg-a/vaydevice_database_sbmv',
 	{
 		pool: {
-			max: 5,
-  			min: 0,
-  			idle: 30000,
-  			acquire: 30000,
+			max: 3,
+			min: 1,
+			idle: 10000,
 		},
 		dialectOptions: {
 			ssl: {

53d79259ec7487bf6e0657f1e636e2dc529cceac
MalikGamaev <malikgamaev@icloud.com>
2025-11-16 13:56:30 +0300

    Update basketDeviceController.js

--- controllers/basketDeviceController.js
+++ controllers/basketDeviceController.js
@@ -67,6 +67,7 @@ class BasketController {
 
   async createPayment(req, res) {
 	const {amount, userId} = req.body
+	console.log(amount, userId)
 	const idempotenceKey = v4().toString();
 	const createPayload = {
     amount: {
